4c45c8309f2a49969b076eddf96f2b64b150ac2a
Benoit Goussen <benoit.goussen@ibacon.com>
2021-07-06 17:32:47 +0200

    Import and adapt relevant functions from the morse package v3.3.1

--- R/odeGUTS-package.R
+++ R/odeGUTS-package.R
@@ -1,8 +1,15 @@
+#' @references
+#'  Virgile Baudrot, Sandrine Charles, Marie Laure Delignette-Muller, Wandrille
+#'  Duchemin, Benoit Goussen, Nils Kehrein, Guillaume, Kon-Kam-King,
+#'  Christelle Lopes, Philippe Ruiz, Alexander Singer and Philippe Veber (2021).
+#'  morse: Modelling Tools for Reproduction and Survival Data in Ecotoxicology.
+#'  R package version 3.3.1. https://CRAN.R-project.org/package=morse
 #' @keywords internal
 "_PACKAGE"
 
 # The following block is used by usethis to automatically manage
 # roxygen namespace tags. Modify with care!
 ## usethis namespace: start
+#' @useDynLib odeGUTS, .registration = TRUE
 ## usethis namespace: end
 NULL
